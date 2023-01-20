bdd48d053ed570972831ea4cfed1de7b9fed4f55
fai227 <fai227@outlook.jp>
2023-01-20 19:18:08 +0900

    Commit

--- make.py
+++ make.py
@@ -1,17 +1,26 @@
-import inflect
 import enchant
 dictionary = enchant.Dict("en_US")
 
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 
-for first in alphabet:
-    for second in alphabet:
-        for third in alphabet:
-            for fourth in alphabet:
-                for fifth in alphabet:
-                    for sixth in alphabet:
-                        for seventh in alphabet:
-                            word = first + second + third + fourth + first + sixth + seventh + fourth
-                            if (dictionary.check(word)):
-                                if (inflect.singular_noun(word) == False):
-                                    print(word)
+word_length = 7
+word_list = [0, 1, 2, 3, 0, 4, 5, 6, 3]
+
+
+def check(num, left_alphabet, words):
+    # 辞書チェック
+    if num > word_length:
+        result = ""
+        for num in word_list:
+            result += words[num]
+        if (dictionary.check(result)):
+            print(result)
+        return
+
+    # 次の文字を調べる
+    for letter in left_alphabet:
+        check(num + 1, left_alphabet.replace(letter, ""), words + letter)
+
+
+print("Starting")
+check(1, alphabet, "")
